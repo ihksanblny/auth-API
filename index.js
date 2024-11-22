bfa59cb74428aa3134daad0c14d79d1a0d86be4e
Naufal Yoga Pratama <naufaltino2113@gmail.com>
2024-11-23 00:01:31 +0700

    setting index.js and connect to MongoDB

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Hello");
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("listening on port " + process.env.PORT);
+});
