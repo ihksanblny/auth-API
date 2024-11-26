9e1ff02ca2798e004adb600f69b5fc4806d9ff01
Naufal Yoga Pratama <naufaltino2113@gmail.com>
2024-11-27 01:41:09 +0700

    create controller, middleware, routes, and hashed function for SignIn

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
-const { signupSchema } = require("../middlewares/validator");
+const jwt = require("jsonwebtoken");
+const { signupSchema, signinSchema } = require("../middlewares/validator");
 const User = require("../models/usersModel");
-const { doHash } = require("../utils/hashing");
+const { doHash, doHashValidation } = require("../utils/hashing");
 
 exports.signup = async (req, res) => {
   const { email, password } = req.body;
@@ -30,6 +31,53 @@ exports.signup = async (req, res) => {
       message: "Your account has been created successfully",
       result,
     });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.signin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const { error, value } = signinSchema.validate({ email, password });
+    if (error) {
+      return res
+        .status(401)
+        .json({ success: false, message: error.details[0].message });
+    }
+
+    const existingUser = await User.findOne({ email }).select("+password");
+    if (!existingUser) {
+      return res
+        .status(401)
+        .json({ success: false, message: "User does not exists!" });
+    }
+    const result = await doHashValidation(password, existingUser.password);
+    if (!result) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid credentials!" });
+    }
+    const token = jwt.sign(
+      {
+        userId: existingUser._Id,
+        email: existingUser.email,
+        verified: existingUser.verified,
+      },
+      process.env.TOKEN_SECRET
+    );
+
+    res
+      .cookie("Auhorization", "Bearer " + token, {
+        expires: new Date(Date.now() + 8 * 3600000),
+        httpOnly: process.env.NODE_ENV === "production",
+        secure: process.env.NODE_ENV === "production",
+      })
+      .json({
+        success: true,
+        token,
+        message: "Logged in successfully",
+      });
   } catch (error) {
     console.log(error);
   }
--- middlewares/validator.js
+++ middlewares/validator.js
@@ -14,3 +14,18 @@ exports.signupSchema = Joi.object({
       new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d@$!%*?&]{8,}$")
     ),
 });
+
+exports.signinSchema = Joi.object({
+  email: Joi.string()
+    .min(6)
+    .max(60)
+    .required()
+    .email({
+      tlds: { allow: ["com", "net", "org"] },
+    }),
+  password: Joi.string()
+    .required()
+    .pattern(
+      new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d@$!%*?&]{8,}$")
+    ),
+});
